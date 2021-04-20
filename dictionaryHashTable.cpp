d2ed649382ffc2792962cbea5fa55b5d08fc70ca
JCab2611 <JCab2611@gmail.com>
2021-04-19 20:38:00 -0400

    Updated to account for edit distance.
    Tweaked the find function to account when a word is not in the dictionary, as well as a display toggle for testing mode.

--- dictionaryHashTable.cpp
+++ dictionaryHashTable.cpp
@@ -2,14 +2,16 @@
 #include <fstream>
 #include <string>
 #include <unordered_set>
+#include <cmath>
 #include "EditDist.h"
+
 using namespace std;
-class dictionaryHashTable{
+class dictionaryHashTable {
     unordered_set<string> words;
 public:
-    dictionaryHashTable(ifstream &file)
+    dictionaryHashTable(ifstream& file)
     {
-        while(!file.eof())
+        while (!file.eof())
         {
             string word;
             getline(file, word);
@@ -20,12 +22,41 @@ public:
     {
         return words;
     }
-    bool found(string &word)
+    void found(string& word, bool display = true)
     {
-        if (words.count(word)) return true;
-        else return false;
+        if (words.count(word)) {
+            if (display)
+                cout << word << " is in the dictionary" << endl;
+        }
+
+        else {
+            if (display)
+                cout << word << " is not in the dictionary, did you mean: ";
+
+            vector<string> pool;
+            float minDist = INFINITY;
+
+            for (auto itr = words.begin(); itr != words.end(); itr++) {
+                float dist = editDistance(*itr, word, 1, 1, 2);
+                if (dist < minDist) {
+                    minDist = dist;
+                    pool.clear();
+                    pool.push_back(*itr);
+                }
+                else if (dist == minDist)
+                    pool.push_back(*itr);
+            }
+
+            if (!display)
+                return;
+
+            for (unsigned int i = 0; i < pool.size(); i++) {
+                cout << pool[i] << "? ";
+            }
+            cout << "\n";
+        }
     }
-   
+
 };
 
 
